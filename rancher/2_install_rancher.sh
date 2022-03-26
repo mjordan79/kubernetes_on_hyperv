7423d8bd163f5e8db4727e5e59664401e240555f
root <root@config.digitalnucleus.it>
2022-03-26 01:38:22 +0100

    Various modifications and improvements

--- rancher/2_install_rancher.sh
+++ rancher/2_install_rancher.sh
@@ -3,8 +3,8 @@ kubectl create namespace cattle-system && \
 	helm repo add rancher-stable https://releases.rancher.com/server-charts/stable && \
 	helm install rancher rancher-stable/rancher \
   	--namespace cattle-system \
-  	--set hostname=host.domain.it \
+  	--set hostname=hostname.domain.it \
   	--set bootstrapPassword=admin \
   	--set ingress.tls.source=secret \
-  	--set privateCA=true
+  	--set privateCA=false
 
